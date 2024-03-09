1065b14bcd5b9b22707e7dc8d584ac4a2754a188
SteveBanks0303 <stephenbankole03@gmail.com>
2024-03-09 11:36:36 +0100

    update

--- React Admin Dashboard/vite.config.js
+++ React Admin Dashboard/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/React-Practice/', // Replace 'repository-name' with your repository name
+  plugins: [react()]
 })
